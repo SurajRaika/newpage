8f4c267242696db991fe1fa7c45e2df41009414f
Suraj Raika <surajraika5sr@gmail.com>
2025-08-01 21:32:43 +0530

    Astro flicked flixed

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx}', // adjust as needed
+  ],
+  theme: {
+    extend: {
+      colors: {
+        custombg: '#1A2238', // change this hex code to your preferred color
+      },
+    },
+  },
+  plugins: [],
+}

eaaae2cb9962426b2e98585e2c373f459754db29
Suraj Raika <surajraika5sr@gmail.com>
2025-08-06 12:26:34 +0530

    maf

---
+++ src/components/DynamicGreeting/events/offlineEvent.ts
@@ -0,0 +1,67 @@
+// Internet Not Found / Offline
+// Image URL: i.pinimg.com/originals/6b/3d/ae/6b3dae013ed1d5fd200327692d33b6f9.png
+// {
+// "src": "i.pinimg.com/originals/6b/3d/ae/6b3dae013ed1d5fd200327692d33b6f9.png",
+// "position": ["bottom-right"],
+// "size": "medium",
+// "text": "The page just... won't load. Is the Wi-Fi down again?"
+// },
+// {
+// "src": "i.pinimg.com/originals/6b/3d/ae/6b3dae013ed1d5fd200327692d33b6f9.png",
+// "position": ["bottom-right"],
+// "size": "medium",
+// "text": "Seriously? Offline? Now?"
+// },
+// {
+// "src": "i.pinimg.com/originals/6b/3d/ae/6b3dae013ed1d5fd200327692d33b6f9.png",
+// "position": ["bottom-right"],
+// "size": "medium",
+// "text": "This swirling loading icon is my nemesis."
+// },
+// {
+// "src": "i.pinimg.com/originals/6b/3d/ae/6b3dae013ed1d5fd200327692d33b6f9.png",
+// "position": ["bottom-right"],
+// "size": "medium",
+// "text": "Maybe if I click it again... nope."
+// },
+// {
+// "src": "i.pinimg.com/originals/6b/3d/ae/6b3dae013ed1d5fd200327692d33b6f9.png",
+// "position": ["bottom-right"],
+// "size": "medium",
+// "text": "Is it my computer? Is it the internet? The eternal question."
+// }
+// ]
+
+
+// [
+//   {
+//     "src": "https://i.pinimg.com/originals/02/51/07/02510742b39f0b2e1040993a5dcc0c73.png",
+//     "position": ["bottom-left"],
+//     "size": "medium",
+//     "text": "The internet is gone... and so is my will to live."
+//   },
+//   {
+//     "src": "https://i.pinimg.com/originals/02/51/07/02510742b39f0b2e1040993a5dcc0c73.png",
+//     "position": ["bottom-left"],
+//     "size": "medium",
+//     "text": "My connection is as dead as I feel right now."
+//   },
+//   {
+//     "src": "https://i.pinimg.com/originals/02/51/07/02510742b39f0b2e1040993a5dcc0c73.png",
+//     "position": ["bottom-left"],
+//     "size": "medium",
+//     "text": "I've tried everything. I give up."
+//   },
+//   {
+//     "src": "https://i.pinimg.com/originals/02/51/07/02510742b39f0b2e1040993a5dcc0c73.png",
+//     "position": ["bottom-left"],
+//     "size": "medium",
+//     "text": "Looks like the world is offline today."
+//   },
+//   {
+//     "src": "https://i.pinimg.com/originals/02/51/07/02510742b39f0b2e1040993a5dcc0c73.png",
+//     "position": ["bottom-left"],
+//     "size": "medium",
+//     "text": "Just let me be sad in the corner with no Wi-Fi."
+//   }
+// ]
